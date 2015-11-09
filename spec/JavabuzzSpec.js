2b1cc9a0f190689f58e33f9706b2bc86d03690ad
mateja683 <mateja683@gmail.com>
2015-11-09 16:25:15 +0000

    adding rspec for 5

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,17 +2,31 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
     it("divsible by 3", function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is not', function() {
     it('divisible by 3', function(){
-      javabuzz = new Javabuzz();
-     expect(javabuzz.isDivisibleByThree(1)).toBe(false); 
-    }); 
+     expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function() {
+    it("divisible by 5", function () {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is not', function() {
+    it("divisible by 5", function () {
+      expect(javabuzz.isDivisibleByFive(4)).toBe(false);
+    });
   });
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -2,6 +2,5 @@ var Javabuzz = function() {
 };
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
+  return (number % 3 === 0);
 };
-
